7c0929e4146ea34fc4ca10c8488a1648922f4f9a
Dovid Levine <david@axepress.dev>
2023-10-01 15:21:39 +0300

    ci: manual rerelease trigger (#8)
    * ci: manual rerelease trigger
    
    * ci: cleanup conditional logic
    
    * ci: strip and sort versions
    
    * Update release-latest-versions.sh
    
    Co-authored-by: Viktor Szépe <viktor@szepe.net>
    
    ---------
    
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- release-latest-versions.sh
+++ release-latest-versions.sh
@@ -2,19 +2,23 @@
 
 set -e
 
+# Get --should-rerelease flag
+if [ "$1" == "--should-rerelease" ]; then
+ SHOULD_RERELEASE=true
+fi
+
 do_release() {
 	local VERSION="$1"
+	local REPACK_VERSION="$2"
 	local SED_EXP
+	
+	VERSION="${VERSION#v}"
 
-	echo "Releasing ${VERSION} ..."
+	echo "Releasing v${VERSION}..."
 
-	# Get new version
-
-	## replace the version of wpackagist-plugin/wp-graphql in composer.json with $VERSION
-
+	# replace the version of wpackagist-plugin/wp-graphql in composer.json with $VERSION
 	contents="$(jq --arg version $VERSION '.require."wpackagist-plugin/wp-graphql" = $version' < source/composer.json)" && \
 	echo -E "${contents}" > source/composer.json
-
 
 	echo "$(cat source/composer.json)"
 	composer update --working-dir=source
@@ -24,8 +28,21 @@ do_release() {
 	composer run-script generate
 
 	# Tag version
-	git commit --all -m "Generate stubs for WPGraphQL ${VERSION}"
-	git tag "v${VERSION}"
+	if [ -z "${REPACK_VERSION}" ]; then
+		echo "Tagging v${VERSION}"
+		git commit --all -m "Generate stubs for WPGraphQL v${VERSION}"
+		git tag "v${VERSION}"
+	else
+		echo "Tagging v${VERSION}+repack.${REPACK_VERSION}"
+		git commit --all -m "Generating stubs for WPGraphQL v${VERSION}+repack.${REPACK_VERSION}"
+		git tag "v${VERSION}+repack.${REPACK_VERSION}"
+
+		# Delete the old git tag.
+	  echo "Deleting old tag v${VERSION}"
+		git tag -d "v${VERSION}"
+		git push --delete origin "v${VERSION}"
+	fi
+
 	git push
 	git push --tags
 	git pull
@@ -35,34 +52,44 @@ do_release() {
 
 GQL_JSON="$(wget -q -O- "https://packagist.org/packages/wp-graphql/wp-graphql.json")"
 
-for MINOR in 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.22 1.23 1.24 1.25; do
-	# Find latest version
-	printf -v JQ_FILTER '.package.versions[].version | select(test("^v%s\\\\.%s\\\\.\\\\d+$"))' "${MINOR%.*}" "${MINOR#*.}"
-	PATCHES="$(jq -r "$JQ_FILTER" <<<"$GQL_JSON" | sort -t "." -k 3 -g)"
-	echo ${PATCHES}
+# Get every possible version from GQL_JSON. Valid versions are prefixed with `v`, followed by anything.
+printf -v JQ_FILTER '.package.versions[].version | select(. | startswith("v"))'
+# Sort the versions.
+POSSIBLE_VERSIONS="$(jq -r "$JQ_FILTER" <<<"$GQL_JSON" | sort -t. -k1,1n -k2,2n -k3,3n -k4,4n)"
 
-	if [ -z "${PATCHES}" ]; then
-		echo "No version for ${MINOR}!"
-		echo
+# Loop through all possible versions and see if they exist as tags
+for POSSIBLE_VERSION in ${POSSIBLE_VERSIONS}; do
+	# Skip versions before 1.0.0
+	if [ "${POSSIBLE_VERSION#v}" \< "1.0.0" ]; then
+		echo "Skipping ${POSSIBLE_VERSION}"
 		continue
 	fi
 
-	for PATCH in ${PATCHES}; do
-		echo ${PATCH}
+	echo "Checking ${POSSIBLE_VERSION}"
 
-		# v1.13.3 was never released
-		if [ "${PATCH}" == "v1.13.3" ]; then
-			echo "Bad release. Skipping ${PATCH}"
+	# If the tag exists, skip it or rerelease it.
+	if git rev-parse "refs/tags/${POSSIBLE_VERSION}" >/dev/null 2>&1; then
+		echo "Tag exists!"
+
+		# 
+		if [ -z "${SHOULD_RERELEASE}" ]; then
+			echo "Skipping"
 			continue
 		fi
 
-		echo $( git rev-parse "refs/tags/${PATCH}" )
-		if git rev-parse "refs/tags/${PATCH}" >/dev/null 2>&1; then
-			echo "Tag exists!"
-			echo
-			continue
-		fi
+		# If SHOULD_RERELEASE is true, append -r1, -r2, etc.
+		for i in {1..100}; do
+			# If the tag doesn't exist, release it
+			echo "Rereleasing as ${POSSIBLE_VERSION}+repack.${i}"
+			if ! git rev-parse "refs/tags/${POSSIBLE_VERSION}+repack.${i}" >/dev/null 2>&1; then
+				do_release "${POSSIBLE_VERSION}" "${i}"
+				break
+			fi
+		done
 
-		do_release "${PATCH//v/}"
-	done
+		continue
+	fi
+
+	# If the tag doesn't exist, release it
+	do_release "${POSSIBLE_VERSION}"
 done
